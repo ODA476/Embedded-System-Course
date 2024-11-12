d60e1c66c552b449a42ac529b8ab3a68e97c4910
ODA476 <omk4762002@gmail.com>
2024-11-12 13:38:02 +0200

    Homework #1

---
+++ HW_1/HW_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "My Name is ODA KARAM" << endl;
+	cout << "My university ID is 120200466" << endl;
+	cout << "This course is Embedded System" << endl;
+	cout << "By Dr.Ahmad Abudabbous" << endl;
+	cout << "This is first homework at this course" << endl;
+
+	return 0;
+}
